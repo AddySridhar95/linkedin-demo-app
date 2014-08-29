aecbb50b3af8d9f97b3894c055806b158b49a74f
Aditya Sridhar <aditya123ss@gmail.com>
2014-08-29 06:37:21 +0530

    route to extrat access_token

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def home():
 @app.route("/linkedin_authenticate")
 def linkedin_authenticate():
 	print("authenticated")
+	print(request.url)
+	url = request.url
+	authentication_code = url.split("=")[1].split("&")[0]
+	print(authentication_code)
 	return render_template('index.html')
 if __name__ == "__main__":
     app.run()
